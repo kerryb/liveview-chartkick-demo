b2040179b34b2438f17a607aca01ecd1ec6f90a8
Kerry Buckley <kerryjbuckley@gmail.com>
2022-06-23 22:44:24 +0100

    Add show/hide buttons (chart doesn’t render on update)

--- lib/my_app_web/live/page_live.ex
+++ lib/my_app_web/live/page_live.ex
@@ -2,13 +2,31 @@ defmodule MyAppWeb.PageLive do
   use MyAppWeb, :live_view
 
   @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, show: false)}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
-    <div id="chart" phx-update="ignore">
-      <%= "{foo: 1, bar: 4, baz: 2, qux: 3}"
-      |> Chartkick.bar_chart()
-      |> Phoenix.HTML.raw() %>
-    </div>
+    <input type="button" phx-click="hide" value="Hide" disabled={not @show} />
+    <input type="button" phx-click="show" value="Show" disabled={@show} />
+    <%= if @show do %>
+      <div id="chart" phx-update="ignore">
+        <%= "{foo: 1, bar: 4, baz: 2, qux: 3}"
+        |> Chartkick.bar_chart()
+        |> Phoenix.HTML.raw() %>
+      </div>
+    <% end %>
     """
+  end
+
+  @impl true
+  def handle_event("hide", _params, socket) do
+    {:noreply, assign(socket, show: false)}
+  end
+
+  def handle_event("show", _params, socket) do
+    {:noreply, assign(socket, show: true)}
   end
 end
